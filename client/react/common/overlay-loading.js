734f435dff712814dadd08f38faedd639a78d4dd
Tuan Anh <ncq998@gmail.com>
2019-10-05 11:08:34 +0700

    dasd34343

---
+++ client/react/common/overlay-loading.js
@@ -0,0 +1,13 @@
+export const OverlayLoading = ({renderText = () => "Loading..."}) => (
+    <div id="initial-loading">
+        <div className="lds-css ng-scope">
+            <div id="il-wrapper">
+                <div style={{width: "100%", height: "100%"}} className="lds-eclipse">
+                    <div>
+                    </div>
+                </div>
+                <p id="il-text">{renderText()}</p>
+            </div>
+        </div>
+    </div>
+);
--- client/react/routes/routes.jsx
+++ client/react/routes/routes.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, {lazy, Suspense} from "react";
 import {Route, Switch, Router, Redirect} from "react-router-dom"
 import { createBrowserHistory } from 'history';
+import {ModalsRegistry} from "../common/modal/modals";
 export const customHistory = createBrowserHistory();
+import {WithLocationRoute} from "./route-types/with-location-route";
+import {AuthenRoute, GuestRoute} from "./route-types/authen-routes";
+import {OverlayLoading} from "../common/overlay-loading";
+const HomeRoute =  lazy(delayLoad(() => import("./common-route/home-route/home-route")));
 
+
+const delayLoad = fn => () => new Promise(resolve => {
+    setTimeout(() => resolve(fn()), 300)
+});
 
 export class MainRoute extends React.Component{
     constructor(props){
@@ -12,13 +21,15 @@ export class MainRoute extends React.Component{
     render(){
         return(
             <div id="main-route">
-
+                    <ModalsRegistry/>
                     <Router
                         history={customHistory}
                     >
+                        <Suspense fallback={<OverlayLoading/>}>
                         <Switch>
-                            <Route exact path={"/"} component={ViewPoolRoute}/>
+                            <WithLocationRoute exact path="/" render={props => (<HomeRoute {...props}/>)}/>
                         </Switch>
+                        </Suspense>
                     </Router>
 
             </div>
