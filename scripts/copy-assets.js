d644e26d6b720c8167e839e148f86bf8c858c5e3
TuanAnh <ncq998@gmail.com>
2019-10-21 16:23:37 +0700

    dasdsa321

--- scripts/copy-assets.js
+++ scripts/copy-assets.js
@@ -6,6 +6,6 @@ const source = "../public";
 const dest = process.env.STATIC_DIR || "build";
 
 Promise.all(listDirFiles.map(item => fs.copy(path.resolve(__dirname, source + "/" + item), path.resolve(__dirname,"../" + dest + "/" + item), {overwrite: true})))
-    .then(() => console.log("success"))
+    .then(() => console.log("Copying asset files successful!"))
     .catch((err) => console.log(err))
 ;
